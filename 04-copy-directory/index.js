361189c139d0e0417c79bcff88b8ab62547e7f39
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-05-20 19:23:08 +0300

    refactor: 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,32 +3,40 @@
 const fsPromises = require('fs/promises');
 const path = require('path');
 
-async function copyDir (dirPath) {
+async function copyDir (dirPath, copiedDirPath) {
   const dir = path.basename(dirPath);
   const parentDir = path.dirname(dirPath);
-  const copiedDirPath = path.resolve(parentDir, dir + '-copy');
 
-  //   await fsPromises.access(copiedDirPath, true)
-  //   await fsPromises.stat(copiedDirPath)
-  //     .then(
-  await fsPromises.rm(copiedDirPath,  {recursive: true})
-    .then(() => {console.log('Old folder was removed');})
-    .catch(() => {console.log('Folder doesn\'t exist!');});
+  if (!copiedDirPath) copiedDirPath = path.resolve(parentDir, dir + '-copy');
+
+  await fsPromises.rm(copiedDirPath,  {recursive: true, force: true})
+    // .then(() => {console.log('Old folder was removed');})
+    .catch(() => {});
 
   await fsPromises.mkdir(copiedDirPath, {recursive: true})
-    .then(() => {console.log('Folder was created');})
+    // .then(() => {console.log('Folder was created');})
     .catch((err) => {console.log('Opss! Here is an Error 00002:', err);});
 
   await fsPromises.readdir(dirPath)
     .then( (data) => {
-    //   console.log(data);
       for (const elem of data) {
-        // console.log(elem);
-        fsPromises.copyFile(path.join(dirPath, elem), path.join(copiedDirPath, elem));
+        const oldFilePath = path.join(dirPath, elem);
+        const newFilePath = path.join(copiedDirPath, elem);
+        fsPromises.stat(oldFilePath)
+          .then((stats) => {
+            if (!stats.isFile()) {
+              console.log('FOLDER:', path.parse(elem).base);
+              copyDir(oldFilePath, newFilePath);
+            } else {
+              console.log('-', path.parse(elem).base);
+              fsPromises.copyFile(oldFilePath, newFilePath);
+            }
+          });
       }
     })
-    .then(() => {console.log('Files was copied');})
+    // .then(() => {console.log('Files was copied');})
     .catch((err) => {console.log('Opss! Here is an Error 00003:', err);});
+  return;
 }
 
-copyDir(path.join(__dirname, 'files'));
+copyDir(path.join(__dirname, 'files'), path.join(__dirname, '..', 'FOLDER'));
