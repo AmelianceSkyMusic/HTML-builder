9ac56c37fbeaf2ca64fa261ebe3b61dcb417007a
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-05-23 00:03:06 +0300

    refactor: 01

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -6,11 +6,6 @@ const fs = require('fs');
 
 const textFielPath = path.join(__dirname, 'text.txt');
 
-// fs.readFile(textFielPath, 'utf-8', (err, data) => {
-//   if (err) throw new Error(err);
-//   console.log(data);
-// });
-
 const readableStream =  fs.createReadStream(textFielPath, 'utf-8');
 let data = '';
 readableStream.on('data', chunk => data += chunk);
